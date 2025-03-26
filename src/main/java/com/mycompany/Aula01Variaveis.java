ef4c345cd9dc802524b7b9d69b94533cca7d2333
Leonardo Roberto Candido <52548402+leonardovere@users.noreply.github.com>
2025-03-26 13:50:55 +0000

    aula 01 de variáveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+    public static void main(String[] args) {
+        // Varíavel lógica
+
+        // Tipo primitivo
+        boolean valorLogicoPrimitivo = true;
+
+        // Tipo abstrato
+        Boolean valorLogicoAbstrato = false;
+    }    
+}
