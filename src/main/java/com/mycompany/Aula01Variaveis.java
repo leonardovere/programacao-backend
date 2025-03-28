b4082f1614d415b361a3dddbf77691d058f2e3ef
Leonardo Roberto Candido <52548402+leonardovere@users.noreply.github.com>
2025-03-28 13:54:38 +0000

    Aula 2 sobre variáveis

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -13,5 +13,29 @@ public class Aula01Variaveis {
         Boolean valorLogicoAbstrato = false;
         // Exibe com texto
         System.out.println("O valor da varíavel valorLogicoAbstrato é: " + valorLogicoAbstrato);
+
+        // Variáveis inteiras
+
+        // Tipo primitivo
+        int valorInteiro = 10; // 32bits
+        long valorInteiroLongo = 10L; // 64bits
+
+        // Tipo abstrato
+        Integer valorInteiroAbs = 20;
+        Long valorLongoAbs = 20L;
+
+        System.out.println("O valor de valorInteiro é: " + valorInteiro + ", e o valor de valorInteiroLongo é: " + valorInteiroLongo);
+
+        // Variáveis Decimais
+
+        // Tipo primitivo
+        float numeroFloat = 10.5f; // 32bits
+        double numeroDouble = 10.5; // 64 bits
+
+        //Tipo abstrato
+        Float numeroFloatAbs = 10.5f;
+        Double numeroDoubleAbs = 10.5;
+
+        System.out.println("O valor de numeroFloat é: " + numeroFloat + ", e o valor de numeroDouble é: " + numeroDouble);
     }    
 }
