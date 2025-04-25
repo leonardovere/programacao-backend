f19894038e4973a26ca718b572805c02b168cd63
Leonardo Roberto Candido <52548402+leonardovere@users.noreply.github.com>
2025-04-25 13:56:28 +0000

    Aula 05 Repetição

--- src/main/java/com/mycompany/Aula05Repeticao.java
+++ src/main/java/com/mycompany/Aula05Repeticao.java
@@ -1,7 +1,27 @@
 package com.mycompany;
 
 public class Aula05Repeticao {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        /**
+         * For (Para)
+         * 
+         * Para cada elemento repita
+         * 
+         * 1° variável de iteração
+         * 2° condição de parada
+         * 3° incremento ou decremento
+         */
+        // Incremento
+        for(int i = 0;i < 10; i = i +2) {
+            System.out.println(i);
+        }
+
+        // Decremento
+        System.out.println("Contagem regressiva...");
+        for (int i = 10; i >= 0; i--) {
+            Thread.sleep(1000L);
+            System.out.println(i + "...");
+        }
+        System.out.println("Booom !!");
     }
 }
