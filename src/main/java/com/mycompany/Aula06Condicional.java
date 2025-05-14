1ef87c6f43a962ff8858da92a3266e3821bfaa9e
Leonardo Roberto Candido <52548402+leonardovere@users.noreply.github.com>
2025-05-14 18:14:11 +0000

    Parte 2 da aula sobre condicionais

--- src/main/java/com/mycompany/Aula06Condicional.java
+++ src/main/java/com/mycompany/Aula06Condicional.java
@@ -1,41 +1,92 @@
 package com.mycompany;
 
+import java.util.Scanner;
+
 public class Aula06Condicional {
     public static void main(String[] args) {
         /**
          * Estrutura condicional simples (if, else)
          * 
-         * pode ser lida como: SE, SENÃO 
+         * pode ser lida como: SE, SENÃO
          * 
          * a condição verdadeira é executada dentro do if
          */
-        int num = 3;
+        /* int num = 3; */
 
         // % é o resto da divisão
         // comparando com 0, significa que o número é par
-        if (num % 2 == 0) {
-            System.out.println(num + " é par !!");
-        } else {
-            System.out.println(num + " é ímpar !!");
-        }
+        /*
+         * if (num % 2 == 0) {
+         * System.out.println(num + " é par !!");
+         * } else {
+         * System.out.println(num + " é ímpar !!");
+         * }
+         */
 
         /**
          * Estrutura condicional composta (if, else if, else)
          * 
-         * nesse tipo de estrutura podemos ter mais de uma 
+         * nesse tipo de estrutura podemos ter mais de uma
          * verificação caso o primeiro if não seja satisfeito
          * 
          * um exemplo prático pode ser a verificação do IMC
          */
 
-        double imc = 24.0;
+        /*
+         * double imc = 24.0;
+         * 
+         * if (imc < 18.5) {
+         * System.out.println("Está abaixo do peso");
+         * } else if (imc >= 18.5 && imc < 24.5) {
+         * System.out.println("Está com peso ideal");
+         * } else {
+         * System.out.println("Está com obesidade");
+         * }
+         */
 
-        if (imc < 18.5) {
-            System.out.println("Está abaixo do peso");
-        } else if (imc >= 18.5 && imc < 24.5) {
-            System.out.println("Está com peso ideal");
-        } else {
-            System.out.println("Está com obesidade");
+        /**
+         * Estrutura Switch/Case
+         * 
+         * É similar ao if/else, com a diferença que ele considera valores predefinidos
+         * que serão observados em cada case (Caso)
+         * 
+         * na sua estrura ele analisa uma variável, e considera dentre suas opções qual
+         * considera.
+         * cada estrutura case deve conter um break, para encerrar a execução.
+         * 
+         * o default (padrão) é executado, caso nenhum case tenha sido executado.
+         * 
+         * um exemplo seria uma pergunta de múltipla escolha.
+         */
+
+        // Digitando textos em mais de uma linha
+        System.out.println(
+                "1) Verde, Amarelo e Azul, são cores que fazem parte da bandeira de qual país?\n" + // Separados por +
+                        "\na) Argentina" + // \n é um caracter de scape usado para pular linha.
+                        "\nb) Paraguai" +
+                        "\nc) Brasil" +
+                        "\nd) Alemanha" + 
+                        "\n\nDigite uma opção: ");
+
+        Scanner scan = new Scanner(System.in);
+        String opcao = scan.nextLine();
+
+        switch (opcao) {
+            case "a":
+                System.out.println("Resposta incorreta !!");
+                break;
+            case "b":
+                System.out.println("Resposta incorreta !!");
+                break;
+            case "c":
+                System.out.println("Resposta correta !!");
+                break;
+            case "d":
+                System.out.println("Resposta incorreta !!");
+                break;
+            default:
+                System.out.println("Opção inexistente !!");
+                break;
         }
     }
 }
---
+++ src/main/java/com/mycompany/exercicios/Ex03.java
@@ -0,0 +1,42 @@
+package com.mycompany.exercicios;
+
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("------ Cálculo de IMC ------");
+        System.out.println("Digite seu peso: ");
+        double peso = scan.nextDouble();
+
+        System.out.println("\nDigite sua altura: ");
+        double altura = scan.nextDouble();
+
+        double imc = peso / Math.pow(altura, 2);
+
+        System.out.print("\nSeu IMC é " + imc + ", ");
+
+        if (imc < 16) {
+            System.out.println("Você está com Magreza Grau III");
+        } else if (imc < 17) {
+            System.out.println("Você está com Magreza Grau II");
+        } else if (imc < 18.5) {
+            System.out.println("Você está com Magreza Grau I");
+        } else if (imc < 25) {
+            System.out.println("Você está com Peso Ideal");
+        } else if (imc < 30) {
+            System.out.println("Você está com Sobrepeso");
+        } else if (imc < 35) {
+            System.out.println("Você está com Obesidade Grau I");
+        } else if (imc < 40) {
+            System.out.println("Você está com Obesidade Grau II");
+        } else {
+            System.out.println("Você está com Obesidade Grau III");
+        }
+
+        System.out.println("\n----- Programa encerrado !! -----");
+
+        scan.close();
+    }
+}
