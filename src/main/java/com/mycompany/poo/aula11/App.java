0d9584123708e0bd5660b5b0f6ea98f0f1ce34e4
Leonardo Roberto Candido <52548402+leonardovere@users.noreply.github.com>
2025-08-27 13:56:20 +0000

    Aula 11, encapsulamento

---
+++ src/main/java/com/mycompany/poo/aula11/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.poo.aula11;
+
+public class App {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        // Modificar o valor
+        pessoa.setNome("Leonardo");
+        pessoa.setIdade(26);
+
+        // Acessar o valor
+        System.out.printf("%s tem %d anos de idade\n", 
+        pessoa.getNome(), pessoa.getIdade());
+    }
+}
