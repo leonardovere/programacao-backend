43f65a36c15580110baf11a50854e74c5a1a1c63
Leonardo Roberto Candido <52548402+leonardovere@users.noreply.github.com>
2025-11-07 13:19:32 +0000

    Herança e Abstração, parte 3

--- src/main/java/com/mycompany/poo/aula13/App.java
+++ src/main/java/com/mycompany/poo/aula13/App.java
@@ -1,6 +1,7 @@
 package com.mycompany.poo.aula13;
 
 import com.mycompany.poo.aula13.animais.Animal;
+import com.mycompany.poo.aula13.animais.Golfinho;
 import com.mycompany.poo.aula13.animais.Leao;
 
 public class App {
@@ -15,5 +16,13 @@ public class App {
         System.out.printf("%s é um animal da espécie Leão de %.1fm de altura, e pesa %.1fKg\n",
                 leao.getNome(), leao.getAltura(), leao.getPeso());
         leao.emitirSom();
+        leao.mover();
+
+        Animal golfinho = new Golfinho();
+        golfinho.setNome("Willy");
+        golfinho.setAltura(1.97);
+        golfinho.setPeso(80);
+        golfinho.emitirSom();
+        golfinho.mover("Sul");
     }
 }
--- src/main/java/com/mycompany/poo/aula13/animais/Golfinho.java
+++ src/main/java/com/mycompany/poo/aula13/animais/Golfinho.java
@@ -7,4 +7,13 @@ public class Golfinho extends Animal {
         System.out.println("O golfinho está grunhindo... ");
     }
 
+    @Override
+    public void mover() {
+        System.out.println(this.getNome() + " está nadando... ");
+    }
+
+    @Override
+    public void mover(String direcao) {
+        System.out.println(this.getNome() + " está nadando para o(a) " + direcao + " ...");
+    }
 }
